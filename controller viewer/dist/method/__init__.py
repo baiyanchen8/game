5958999958c5a6b911497948cc597e6e8c0aa3f2
baiyanchen8 <132753198+baiyanchen8@users.noreply.github.com>
2024-02-03 18:16:39 +0800

    new

---
+++ controller viewer/dist/method/__init__.py
@@ -0,0 +1,10 @@
+# this is method package to pac-man(ghost capture) game 
+# last edit time 2023/5/17
+# from editor chen po yen 
+
+# import method 
+
+from  . import img_path
+from  . import draw_text
+# from method import *
+__all__ = ["img_path"]
---
+++ controller viewer/dist/method/img_path.py
@@ -0,0 +1,16 @@
+# there is where to place path to img
+font_path={
+    "retro":"font\Retro.ttf",
+    "keyboard":"font/font_keyboard.ttf"
+}
+pic={
+    "controller":"pic/controller-1827840_640.png",
+    "lt_frame":"pic/lt.png",
+    "rt_frame":"pic/rt.png",
+    "RB":"pic/rb.png",
+    "LB":"pic/lb.png",
+    "DD":"pic/newdown.png",
+    "DU":"pic/newup.png",
+    "DL":"pic/newleft.png",
+    "DR":"pic/newright.png",
+}
